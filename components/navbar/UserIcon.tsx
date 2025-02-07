f099fcc01d4290db298b0680c6a4de90dfae6a8e
Matic.dev <olaitantoyeeb1@gmail.com>
2025-02-07 20:06:05 +0100

    solving vercel error, and adding height to userIcon

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 async function UserIcon() {
   const profileImage = await fetchProfileImage();
   if (profileImage) {
-    return <Image className='w-6 h-6 rounded-full object-cover' src={profileImage} alt='Profile' />;
+    return <Image className='w-6 h-6 rounded-full object-cover' width={24} height={24} src={profileImage} alt='Profile' />;
   }
   return <LuUser className='w-6 h-6 bg-primary rounded-full text-white' />;
 }
